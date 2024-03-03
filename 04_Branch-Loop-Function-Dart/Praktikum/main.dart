5d8bc7eb414baae2a62bc279e4e1a78a39835ab7
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-03-03 20:03:28 +0700

    ✨ feat: Branching

---
+++ 04_Branch-Loop-Function-Dart/Praktikum/main.dart
@@ -0,0 +1,22 @@
+String kategoriNilai(int nilai) {
+  if (nilai > 70) {
+    return "Nilai A";
+  } else if (nilai > 40) {
+    return "Nilai B";
+  } else if (nilai > 0) {
+    return "Nilai C";
+  } else {
+    return "";
+  }
+}
+
+void main() {
+  int nilai = 0;
+  String kategori = kategoriNilai(nilai);
+
+  if (kategori.isNotEmpty) {
+    print('Kategori nilai untuk $nilai: $kategori');
+  } else {
+    print('Nilai $nilai tidak masuk dalam kategori yang ditentukan.');
+  }
+}
