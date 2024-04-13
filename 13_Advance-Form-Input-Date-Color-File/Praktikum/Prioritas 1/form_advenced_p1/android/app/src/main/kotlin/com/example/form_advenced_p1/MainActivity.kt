24797ffb4af13a69f398c1eb09bcf9e54eee0aad
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-04-13 17:50:37 +0700

    ✨ feat: Add jawaban soal prioritas 1

---
+++ 13_Advance-Form-Input-Date-Color-File/Praktikum/Prioritas 1/form_advenced_p1/android/app/src/main/kotlin/com/example/form_advenced_p1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.form_advenced_p1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- 13_Advance-Form-Input-Date-Color-File/Praktikum/form_advenced/android/app/src/main/kotlin/com/example/form_advenced/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.form_advenced
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
